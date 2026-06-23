33a0ec60aa025c37de82a192df426fd599b8c12d
Kayverr <ferrerkylecedrick@gmail.com>
2026-06-23 10:35:22 +0800

    Edited some files that cause failure

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const LoginButton = ({ onClick, disabled = false }) => {
   return (
     <button
--- frontend/src/components/TextField.jsx
+++ frontend/src/components/TextField.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const TextField = ({
   type = "text",
   placeholder,
